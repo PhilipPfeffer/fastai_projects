bf8b8c937a124ea025cb0d749f44bbd5c051b12e
Philip Mateo Pfeffer <philipmateopfeffer@MacBook-Pro.local>
2020-06-16 16:50:09 -0300

    googleimagesdownload out of date so imgdownload.py deprecated

--- imgdownload_GID_BAD.py
+++ imgdownload_GID_BAD.py
@@ -1,8 +1,13 @@
+##### googleimagesdownload OUT OF DATE ####
+
 #!/usr/bin/python
 
 import sys
 from subprocess import call
 
+
+# Trying to use google_images_download to download images
+# googleimagesdownload -k "button-up shirts" -s medium -l 10 -o "data/clothes_classifier" -i "button-up shirts" -cd "~/../../Applications/chromedriver"
 project_name = "clothes_classifier/"
 search_term = sys.argv[1]
 num_imgs = sys.argv[2]
